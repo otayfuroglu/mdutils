1f755bb6844ad8220a68da0d0a37f9f30c7bc06c
otayfuroglu <tayfuroglu@gmail.com>
2021-08-18 13:06:26 +0300

    added some function

--- automd/ligPrep.py
+++ automd/ligPrep.py
@@ -14,6 +14,9 @@ class ligPrep:
 
     def __init__(self, mol_path):
         self.mol_path = mol_path
+        # get file format 
+        self.init_file_format = self._getFileFormat()
+
 
         # load OB molecule
         self.ob_mol = None
@@ -22,21 +25,16 @@ class ligPrep:
         # initialize RW mol
         self.rw_mol = None
 
-
-        # get file format 
-        self.mol_format = self._getFileFormat()
-
-
     def _getFileFormat(self):
         return self.mol_path.split(".")[-1]
 
     def _loadMol(self):
         # mol = readfile("smi", "myfile.smi").next() # Python 2
         # mol = next(readfile("smi", "myfile.smi"))  # Python 3
-        self.ob_mol = next(pybel.readfile(self.mol_format, self.mol_path))
+        self.ob_mol = next(pybel.readfile(self.init_file_format, self.mol_path))
 
-    #  def addH(self):
-    #       self.ob_mol.addh()
+    def addH(self):
+         self.ob_mol.addh()
 
     #  def removeH(self):
     #       self.ob_mol.removeh()
@@ -59,10 +57,20 @@ class ligPrep:
             fl.write(self.ob_mol)
 
     def writeRWMol2File(self, file_path):
-        if rw_mol is None:
+        import sys
+        if self.rw_mol is None:
             self.obMol2RWmol()
-        rdkit.Chem.rdmolfiles.MolToPDBFile(rw_mol, file_path)
+
+        file_format = file_path.split(".")[-1]
+
+        if file_format == "xyz":
+            rdkit.Chem.rdmolfiles.MolToXYZFile(self.rw_mol, file_path)
+        elif file_format == "pdb":
+            rdkit.Chem.rdmolfiles.MolToPDBFile(self.rw_mol, file_path)
+        else:
+            print("Unknown file format")
+            sys.exit(1)
 
     def printTest(self):
-        print(len(self.ob_mol.atoms))
+        print(len(self._getFileFormat()))
 
