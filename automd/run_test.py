8a997a2dce3741b3d7898f102bda3e5b4be069cf
otayfuroglu <tayfuroglu@gmail.com>
2021-08-18 13:08:32 +0300

    draft test run file

---
+++ automd/run_test.py
@@ -0,0 +1,16 @@
+
+from ligPrep import ligPrep
+mol_path = "./test/article_No13_8.xyz"
+lig = ligPrep(mol_path)
+lig.addH()
+lig.obMol2RWmol()
+lig.writeRWMol2File("test_rw_file.mol2")
+#  lig.removeH()
+#  lig.addH()
+#  lig.convertFileFormat("mol2", "./test/convetedFile.mol2")
+#  lig.writeOBMol2File("mol", "test/test_removeH.mol")
+#  lig.addHWithOB()
+#  lig.writeOBMol2File("mol", "test/test.mol")
+#  lig.getObmolFromRWmol()
+#  lig.writeRWMol2File("./test/test.pdb")
+#  lig.printTest()
