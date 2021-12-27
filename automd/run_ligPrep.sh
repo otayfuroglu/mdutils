e4224020fcd00653962d1e7b50511ad78348253b
otayfuroglu <tayfuroglu@gmail.com>
2021-12-27 20:25:34 +0300

    added add hydrogen func and documentation

--- automd/run_ligPrep.sh
+++ automd/run_ligPrep.sh
@@ -7,8 +7,15 @@ PYTHON_DIR="$HOME/miniconda3/bin"
 
 
 struct_dir=test0
+
+# adding hydrogen if missing (yes/no)
+add_hydrogen=yes
+
+# select caclulator type (ani2x/g16)
 # caculator_type=g16
 caculator_type=ani2x
+
+# performe geometry optimization if desired (yes/no)
 optimization=no
 
 
@@ -23,7 +30,7 @@ num_conformers=50
 max_attempts=1000
 prune_rms_thresh=0.2
 
-$PYTHON_DIR/python $ligPrep_DIR/runLigPrep.py $struct_dir $caculator_type $optimization $thr_fmax $maxiter $num_conformers $max_attempts $prune_rms_thresh
+$PYTHON_DIR/python $ligPrep_DIR/runLigPrep.py $struct_dir $add_hydrogen $caculator_type $optimization $thr_fmax $maxiter $num_conformers $max_attempts $prune_rms_thresh
 
 
 
