a938b0a83959a9bd8b61aa7ef1e2544ea31507d7
otayfuroglu <tayfuroglu@gmail.com>
2021-12-30 19:43:01 +0300

    added some prop

--- automd/ligprep/run_ligPrep.sh
+++ automd/ligprep/run_ligPrep.sh
@@ -1,36 +1,42 @@
 #! /usr/bin/env bash
 
 
-ligPrep_DIR="/cta/users/otayfuroglu/workspace/mdutils/automd"
+ligPrep_DIR="/cta/users/otayfuroglu/workspace/mdutils/automd/ligprep"
 PYTHON_DIR="$HOME/miniconda3/bin"
 
 
 
-struct_dir=test0
+struct_dir=test
 
 # adding hydrogen if missing (yes/no)
 add_hydrogen=yes
 
 # select caclulator type (ani2x/g16)
 # caculator_type=g16
-caculator_type=ani2x
+caculator_type="ani2x"
 
-# performe geometry optimization if desired (yes/no)
-optimization=no
+# perform geometry optimization for conformers if desired (yes/no)
+optimization_conf=no
 
+# perform geometry optimization for orginal ligand if desired (yes/no)
+optimization_lig=yes
 
 # set thrshold fmax for optimization (default=0.01)
-thr_fmax=1000.01
+thr_fmax=0.7
 
 #maximum iteration for optimization
 maxiter=500
 
-#configuration for conformer generator parameters
-num_conformers=50
-max_attempts=1000
-prune_rms_thresh=0.2
+# generate conformer if desired (yes/no)
+genconformer=yes
 
-$PYTHON_DIR/python $ligPrep_DIR/runLigPrep.py $struct_dir $add_hydrogen $caculator_type $optimization $thr_fmax $maxiter $num_conformers $max_attempts $prune_rms_thresh
+#configuration for conformer generator parameters
+num_conformers=5
+max_attempts=1000
+prune_rms_thresh=0.1
+
+$PYTHON_DIR/python $ligPrep_DIR/runLigPrep.py $struct_dir $add_hydrogen $caculator_type\
+	$optimization_conf $optimization_lig $genconformer $thr_fmax $maxiter $num_conformers $max_attempts $prune_rms_thresh
 
 
 
