6adf86212936a49b529a86153e24ef72f69c2321
otayfuroglu <tayfuroglu@gmail.com>
2021-12-31 19:04:42 +0300

    fixed several issues

--- automd/ligprep/runLigPrep.py
+++ automd/ligprep/runLigPrep.py
@@ -1,4 +1,5 @@
 
+from ase.io import read
 from ligPrep import ligPrep
 import argparse
 import os, sys
@@ -48,9 +49,9 @@ num_conformers = args.num_conformers
 max_attempts = args.max_attempts
 prune_rms_thresh = args.prune_rms_thresh
 
-def setG16calculator(lig, file_base, WORK_DIR):
+def setG16calculator(lig, file_base, label, WORK_DIR):
     lig.setG16Calculator(
-            label="%s/g16_calculation/%s"%(WORK_DIR, file_base),
+            label="%s/g16_%s/%s"%(WORK_DIR, label, file_base),
             chk="%s.chk"%file_base,
             xc="HF",
             basis="sto-3g",
@@ -70,23 +71,18 @@ def runLigPrep(file_base):
     if not os.path.exists(WORK_DIR):
         os.mkdir(WORK_DIR)
 
+    #Flags
+    # defaul mm calculator set to False
+    mmCalculator=False
+
     # initialize ligPrep
     lig = ligPrep(mol_path, WORK_DIR)
     #  lig.writeRWMol2File("test/test.xyz")
 
-    # if desire adding H by rdkit
-    if add_hydrogen:
-        lig.addHwithRD()
-
-    # defaul mm calculator set to False
-    mmCalculator=False
-
-    sp4esp = True
     if "ani2x" in calculator_type.lower():
         lig.setANI2XCalculator()
     elif "g16" in calculator_type.lower():
-        sp4esp = False
-        lig = setG16calculator(lig, file_base)
+        lig = setG16calculator(lig, file_base, label="calculation", WORK_DIR=WORK_DIR)
     elif "uff" in calculator_type.lower():
         if optimization_conf:
             print("UFF calculator not support optimization")
@@ -94,17 +90,21 @@ def runLigPrep(file_base):
         else:
             mmCalculator=True
 
+    # if desire adding H by rdkit
+    prefix = ""
+    if add_hydrogen:
+        lig.addHwithRD()
+        prefix += "addH_"
+
     #  if optimization_conf:
     # set optimizetion parameters
     lig.setOptParams(fmax=thr_fmax, maxiter=1000)
 
-
-    prefix = ""
     if optimization_lig or optimization_conf:
-        prefix = "opt_"
-    out_file_path="%s/%sminE_conformer.xyz"%(WORK_DIR, prefix)
+        prefix += "opt_"
     if genconformer:
-        ase_atoms = lig.genMinEGonformer(
+        out_file_path="%s/%sminE_conformer.sdf"%(WORK_DIR, prefix)
+        minE_ase_atoms = lig.genMinEGonformer(
             file_path=out_file_path,
             numConfs=num_conformers,
             maxAttempts=max_attempts,
@@ -117,33 +117,27 @@ def runLigPrep(file_base):
         print("Selected minimun energy conformer")
         if  not optimization_conf and optimization_lig:
             print("Optimization for minumum energy conformer")
-            lig.geomOptimization(ase_atoms)
-            lig.writeAseAtoms(out_file_path)
-        else:
-            lig.writeAseAtoms(out_file_path)
-
+            lig.geomOptimization(minE_ase_atoms)
     else:
+        out_file_path="%s/%s%s.sdf"%(WORK_DIR, prefix, file_base)
         # geometry optimizaton for ligand
         if  optimization_lig:
             ase_atoms = lig.rwMol2AseAtoms()
             lig.geomOptimization(ase_atoms)
-        # ligad coordintes write to xyz file
-        lig.writeAseAtoms(out_file_path)
 
+    lig.writeRWMol2File(out_file_path)
 
-    if sp4esp:
-        from ase.io import read
-        atoms = read(out_file_path)
-
-        lig = setG16calculator(lig, file_base, WORK_DIR)
-        lig.calcSPEnergy(atoms)
+    # g16 calculation for generate ESP chage
+    atoms = read(out_file_path)
+    lig = setG16calculator(lig, file_base, label="esp_calculation", WORK_DIR=WORK_DIR)
+    lig.calcSPEnergy(atoms)
 
 def main():
     file_names = [item for item in os.listdir(structure_dir) if item.endswith(".mol2")]
     for file_name in file_names:
         file_base = file_name.replace(".mol2", "")
         runLigPrep(file_base)
-        os.system("bash ligPrep_utils.sh %s" %file_base)
+        os.system("bash ligPrepHelper.sh %s" %file_base)
 
 if __name__ == "__main__":
     main()
