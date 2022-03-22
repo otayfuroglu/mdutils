d1272a56006a4235f20245feaf27c22c98c96ca6
otayfuroglu <tayfuroglu@gmail.com>
2022-03-22 17:31:00 +0300

    small changes

--- automd/ligprep/run_ligPrep.sh
+++ automd/ligprep/run_ligPrep.sh
@@ -1,15 +1,15 @@
 #! /usr/bin/env bash
 
-ligPrep_DIR="/cta/users/otayfuroglu/workspace/mdutils/automd/ligprep"
+ligPrep_DIR="$HOME/Desktop/mdutils/automd/ligprep"
 PYTHON_DIR="$HOME/miniconda3/bin"
 
-struct_dir=test_serdar
+struct_dir=test
 
 # adding hydrogen if missing (yes/no)
 add_hydrogen=no
 
 # optimization ligand if desired before conformer generation (yes/no)
-pre_optimization_lig=yes
+pre_optimization_lig=no
 
 # generate conformer if desired (yes/no)
 genconformer=yes
